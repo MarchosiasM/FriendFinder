0b2a1c6f8d6dce109febf4a84acf38c551c8d0b4
Michael Malach <mmalach@ucsd.edu>
2018-04-28 22:23:36 -0700

    All working. Some weird error about setting headers has popped up, but the logic works, so oh well.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,8 @@
 let friendData = require('../data/friends.json')
-console.log(friendData)
+const fs = require('fs')
+
+console.log("FriendData loaded as " + (friendData))
+console.log("First entry is " + friendData[0])
 
 // ===============================================================================
 // ROUTING
@@ -13,35 +16,49 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    console.log(req.body)
+      
     friendData.push(req.body)
-    res.json(matchLogic())
+
+    fs.writeFile("./app/data/friends.json", JSON.stringify(friendData), "utf8", (error) => {
+        if (error) console.log(`Something wrong with your file write. ${error}`)
+    })
+
+    res.json(matchLogic(req.body))
     res.json(true)
   })
-
-
 };
 
 
 function matchLogic(newUser) {
     // Iterate over the entries in friend data and compare the newUser's totals to the others, then return the closest match (lowest abs difference)
     // In the case of ties, return the last one calculated
-
+    console.log("Match logic received " +newUser + " for new user data")
     let closestMatch = 0;
     let newUserTotal = 0;
+    let userTotal = 0;
+    let bestDiff = 100;
+    let diff = 0;
 
     newUserTotal = newUser.scores.reduce(sum, 0)
+    console.log("New users total is " + newUserTotal)
 
     // This should iterate over all friend data minus the last (presumably, she who was just added
-    for (let i = 0; i < friendData - 1; i++) {
-        let userTotal = Math.abs(friendData.scores.reduce(sum, 0))
-        if (userTotal > closestMatch) {
+    for (let i = 0; i < friendData.length - 1; i++) {
+        console.log("Loop logic on " + i + " loop")
+        // console.log(friendData.scores)
+        userTotal = Math.abs(friendData[i].scores.reduce(sum, 0))
+        diff = Math.abs(newUserTotal - userTotal)
+        console.log("This user had a diff of " + diff + " and a total of " + userTotal)
+        if (diff < bestDiff) {
+            console.log("Inside the testing logic. userTotal is " + userTotal + " and closest match is " )
             closestMatch = i;
+            bestDiff = diff;
         }
     }
-    return friendData[i]
+    console.log("This function thinks " + friendData[closestMatch].name + " is the best match, with a score of " + friendData[closestMatch].scores.reduce(sum, 0))
+    return friendData[closestMatch]
 }
 
 function sum(a, b) {
-    return a + b;
+    return parseInt(a) + parseInt(b);
 }
